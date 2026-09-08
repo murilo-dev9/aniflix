03a8797f02095350ce3be79183ea95d89223e134
murilo <murl09g@gmail.com>
2026-09-08 11:40:44 -0300

    criando_menu1

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import styles from "./App.module.css";
+import menu from "./componentes/menu"
+
+const home = () => {
+  return(
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/componentes/menu.jsx
@@ -0,0 +1,9 @@
+const menu =()=>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default menu
